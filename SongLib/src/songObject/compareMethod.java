1d8c88d9d41b3d19e1508375607594c62f61acdc
Dhruv Dinesh <32970682+Dalgom02@users.noreply.github.com>
2023-02-21 18:49:07 -0500

    Update compareMethod.java

--- SongLib/src/songObject/compareMethod.java
+++ SongLib/src/songObject/compareMethod.java
@@ -7,15 +7,20 @@ import java.util.Comparator;
 
 public class compareMethod implements Comparator<Song> {
 
-public int compare(Song s1, Song s2) {
-		
-		if(s1.getTitle().toLowerCase().compareTo(s2.getTitle().toLowerCase()) == 0) {
-			
-			return s1.getArtist().toLowerCase().compareTo(s2.getArtist().toLowerCase());
-		} 
-		
-		return s1.getTitle().toLowerCase().compareTo(s2.getTitle().toLowerCase());
+	 @Override
+	    public int compare(Song s1, Song s2) {
+	        int titleCompare = s1.getTitle().toLowerCase().compareTo(s2.getTitle().toLowerCase());
+	        if (titleCompare == 0) {
+	            int artistCompare = s1.getArtist().toLowerCase().compareTo(s2.getArtist().toLowerCase());
+	            if (artistCompare == 0) {
+	                // If both songs have the same title and artist, compare by album
+	                return s1.getAlbum().toLowerCase().compareTo(s2.getAlbum().toLowerCase());
+	            } else {
+	                // If songs have the same title but different artist, compare by artist
+	                return artistCompare;
+	            }
+	        }
+	        // If songs have different titles, compare by title
+	        return titleCompare;
+	    }
 	}
-	
-	
-}
